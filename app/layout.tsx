a94e3eb56bcd822a0934b51d984732cf434c97fc
0xSabdadev <jasonalhilal@gmail.com>
2023-03-19 21:28:27 +0700

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,21 @@
+import Link from 'next/link'
 export default function RootLayout({children}: {children: React.ReactNode}) {
     return (
         <html>
             <head />
             <body>
-                <div>Header</div>
+                <ul>
+                    <li>
+                        <Link href={'/'}>Home</Link>
+                    </li>
+                    <li>
+                        <Link href={'/dashboard'}>Dashboard</Link>
+                    </li>
+                    <li>
+                        {' '}
+                        <Link href={'/setting'}>Setting</Link>
+                    </li>
+                </ul>
                 {children}
                 <div>Footer</div>
             </body>
