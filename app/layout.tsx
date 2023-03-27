7129cb42c95d645e9dde7591115a7bdd3159718b
0xSabdadev <jasonalhilal@gmail.com>
2023-03-27 20:11:51 +0700

    morning add

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,25 +7,27 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
     const onClickUrl = (url: string) => {
         router.push(url)
     }
+    const menuItem = (url: string, title: string, params?: string) => {
+        return (
+            <div className={`rounded-lg bg-black text-white py-2 px-3 ${params}`}>
+                <Link href={url}>{title}</Link>
+            </div>
+        )
+    }
     return (
         <html>
             <head />
-            <body>
-                <ul>
-                    <li>
-                        <Link href={'/'}>Home</Link>
-                    </li>
-                    <li>
-                        <Link href={'/dashboard'}>Dashboard</Link>
-                    </li>
-                    <li>
-                        <Link href={'/settings'}>Setting</Link>
-                    </li>
-                    <li>
-                        <Link href={'/cari'}>Cari USer</Link>
-                    </li>
-                </ul>
-                {children}
+            <body className=' container mx-auto py-[10px]'>
+                {/* menu */}
+                <div className='w-full inline-flex'>
+                    {menuItem('/', 'Home')}
+                    <div className='inline-flex  justify-end space-x-3 w-full'>
+                        {menuItem('/dashboard', 'Dashboard')}
+                        {menuItem('/settings', 'Setting')}
+                        {menuItem('/cari', 'Cari User')}
+                    </div>
+                </div>
+                <div>{children}</div>
                 <div>Footer</div>
             </body>
         </html>
